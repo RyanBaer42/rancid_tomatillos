be8a9c52c783fb7ac624f9cd817fc5a096ad57e9
Ryan Baer <ryanjbaer@outlook.com>
2023-02-02 08:27:42 -0700

    restructure app component and add website title

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MovieFinder from '../../MovieFinder.png'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src={MovieFinder} alt="Website Logo" className='logo'/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
