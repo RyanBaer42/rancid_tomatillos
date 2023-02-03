3909a4846a005310903d39b06ba09218a583cf94
Craig Weller <crg.weller@gmail.com>
2023-02-02 18:31:27 -0700

    add api call function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,72 +1,64 @@
-import React, { Component } from 'react';
-import MoldyTurnips from '../../MoldyTurnips.png'
-import MovieGrid from ".././MovieGrid/MovieGrid.js"
-import MovieBanner from '.././MovieBanner/MovieBanner.js'
+import React, { Component } from "react";
+import MoldyTurnips from "../../MoldyTurnips.png";
+import MovieGrid from ".././MovieGrid/MovieGrid.js";
+import MovieBanner from ".././MovieBanner/MovieBanner.js";
+import movieData from "./MockData.js";
 
-import './App.css';
+import "./App.css";
 
-
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [{
-        "id": 694919,
-        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-        "title": "Money Plane",
-        "average_rating": 6.666666666666667,
-        "release_date": "2020-09-29"
-      },
-      {
-        "id": 337401,
-        "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-        "title": "Mulan",
-        "average_rating": 4.909090909090909,
-        "release_date": "2020-09-04"
-      },
-      {
-        "id": 718444,
-        "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
-        "title": "Rogue",
-        "average_rating": 5.428571428571429,
-        "release_date": "2020-08-20"
-      },
-      {
-        "id": 539885,
-        "poster_path": "https://image.tmdb.org/t/p/original//qzA87Wf4jo1h8JMk9GilyIYvwsA.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//54yOImQgj8i85u9hxxnaIQBRUuo.jpg",
-        "title": "Ava",
-        "average_rating": 5.111111111111111,
-        "release_date": "2020-07-02"
-      },
-      {
-        "id": 581392,
-        "poster_path": "https://image.tmdb.org/t/p/original//sy6DvAu72kjoseZEjocnm2ZZ09i.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//gEjNlhZhyHeto6Fy5wWy5Uk3A9D.jpg",
-        "title": "Peninsula",
-        "average_rating": 7,
-        "release_date": "2020-07-15"
-      },
-    ]}
+      movies: [],
+      singleMovie: {},
+      isLoading: true,
+    };
   }
 
+  // handleClick = event => {
+  //   this.setState({ })
+  // }
+
+  // componentDidMount = () => {
+  //   this.setState({
+  //     movies: movieData.movies,
+  //   })
+  // }
+
+  componentDidMount() {
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw Promise.reject(response);
+      })
+      .then((result) => {
+        this.setState({ movies: result.movies, isLoading: false });
+      })
+      .catch((response) => {
+        this.setState({ error: "Somethin aint workin" });
+      });
+  }
 
   render() {
-    return (
+    if (!this.state.isLoading) {
+      return (
+        <div className="App">
+          <div>
+            <img src={MoldyTurnips} alt="Website Logo" className="logo" />
+          </div>
+          <MovieBanner movies={this.state.movies} />
+          <MovieGrid movies={this.state.movies} />
+        </div>
+      );
+    } else {
       <div className="App">
-      <div>
-        <img src={MoldyTurnips} alt="Website Logo" className='logo'/>
-      </div> 
-      <MovieBanner movies={this.state.movies}/>
-      <MovieGrid movies={this.state.movies} />
-    </div>
-    )
+        <h1>Loading Movies</h1>
+      </div>;
+    }
   }
-
-
 }
 
 export default App;
